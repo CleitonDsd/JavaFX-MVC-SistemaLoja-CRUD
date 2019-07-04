8a42d6abc7e376fef2d01c63fb32e8b177f42454
Cleiton Dias <s.diascleiton@gmail.com>
2019-07-04 20:36:52 -0300

    Inserindo o nome do banco de dados

--- src/javafxmvc/model/database/DatabaseFactory.java
+++ src/javafxmvc/model/database/DatabaseFactory.java
@@ -2,7 +2,7 @@ package javafxmvc.model.database;
 
 public class DatabaseFactory {
     public static Database getDatabase(String nome){
-        if(nome.equals("postgresql")){
+        if(nome.equals("javafxmvc")){
             return new DatabasePostgreSQL();
         }else if(nome.equals("mysql")){
             return new DatabaseMySQL();
